16fb2e5ddddaa66845a7e1b7b46951e84396d15c
Houssnou <hosni.moubarak@gmail.com>
2019-03-14 18:45:00 -0400

    initial commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+  public class DataContext : DbContext
+  {
+    public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+    public DbSet<Value> Values { get; set; }
+  }
+}
