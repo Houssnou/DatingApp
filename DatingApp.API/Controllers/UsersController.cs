bcbeb4256c765420f782046f233c07f811f1de90
Houssnou <hosni.moubarak@gmail.com>
2019-03-22 00:14:20 -0400

    End of section 13

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -34,7 +34,7 @@ namespace DatingApp.API.Controllers
       return Ok(usersToReturn);
     }
 
-    [HttpGet("{id}")]
+    [HttpGet("{id}", Name = "GetUser")]
     public async Task<IActionResult> GetUser(int id)
     {
       var user = await _repo.GetUser(id);
--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.API.Dtos
@@ -8,7 +9,26 @@ namespace DatingApp.API.Dtos
         public string Username { get; set; }
 
         [Required]
-        [StringLength(8, MinimumLength=6, ErrorMessage="Enter a password between 6 and 8 characters length.")]
+        [StringLength(8, MinimumLength=6, ErrorMessage="Enter a password between 6 and 8 characters length.")]        
         public string Password { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string KnownAs { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; } 
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+
+        public UserForRegisterDto()
+        {
+           Created = DateTime.Now;
+           LastActive = DateTime.Now;
+        }
+
     }
 }
